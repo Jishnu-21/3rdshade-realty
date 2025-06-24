485d84a2d49ddf04eabdf5bcfee0110f68db1000
Jishnu <jpjishnu21@gmail.com>
2025-06-24 17:45:27 +0530

    made changes

--- client/src/app/property/[slug]/page.tsx
+++ client/src/app/property/[slug]/page.tsx
@@ -1,5 +1,7 @@
+import { use } from "react";
 import PropertyDetailsClient from './PropertyDetailsClient';
 
-export default function Page({ params }: { params: { slug: string } }) {
-  return <PropertyDetailsClient slug={params.slug} />;
+export default function Page({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = use(params);
+  return <PropertyDetailsClient slug={slug} />;
 }
